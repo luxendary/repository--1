ad00533a752d390c8bb14aeedeee5641f74477df
Kai <nurulloev@gmail.com>
2019-10-28 20:49:12 -0400

    string name,int age,double weight

---
+++ src/main/java/Racoon.java
@@ -0,0 +1,2 @@
+public class Racoon {
+}
---
+++ src/main/java/Zoo.java
@@ -0,0 +1,32 @@
+public class Zoo {
+    String Name;
+    int age;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    double weight;
+
+    }
+}
